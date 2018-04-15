e430ad04191335ac966c890ed4766861eb26b456
vaishnavisuguru <vsuguru@unomaha.edu>
2018-04-15 18:50:18 -0500

    Added Google Translator for Product Reviews and added Landing Page

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+
+
 class Category(models.Model):
 
     name = models.CharField(max_length=200,
@@ -55,3 +57,21 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('shop:product_detail',
                        args=[self.id, self.slug])
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
         url(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
-        url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+        url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/Eng/$', views.product_detail, name='product_detail'),
+        url(r'^(?P<id>\d+)/French/$', views.french, name='French'),
+        url(r'^(?P<id>\d+)/Spanish/$', views.spanish, name='Spanish'),
+        url(r'^(?P<id>\d+)/Hindi/$', views.hindi, name='Hindi'),
+        url(r'^addReview/(?P<id>\d+)/$', views.add_review, name='addReview'),
         #Root url
         url(r'^$', views.product_list, name='product_list'),
         ]
